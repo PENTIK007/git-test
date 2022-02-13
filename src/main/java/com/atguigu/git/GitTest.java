2f6f03a4b2de4ff25d61b5df23cd60ab991065c1
PENTIK007 <67768443+PENTIK007@users.noreply.github.com>
2022-02-13 22:05:17 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hot-fix test2!");
         System.out.println("hello git5!");
         System.out.println("hello git6!");
+        System.out.println("pull test!");
     }
 }
